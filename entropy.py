09f8ebd824cbd8151282d054d0c3bf7e6161eca2
Dustin Raimondi <raimondi1337@gmail.com>
2014-02-27 14:17:28 -0500

    Added a test image and a test algorithm, complete with pretty colors highlighting related lines

--- entropy.py
+++ entropy.py
@@ -24,21 +24,17 @@ def main(argv):
 			_debug = 1
 		elif opt in ("-i", "--image"):
 			image = arg           
-
 	source = "".join(args)
 	#Parse the arguments, then create a new image processor object with the value of the -i or --image argument as the image file to open
 	p = ProcessImage(source)
 	p.output()
-
 #Class to handle image processing
 #It inherits from the generic Python object
 class ProcessImage(object):
-
 	def __init__(self, source): #Pass in the argument image location
 		#get the location of the image
 		self.source = source
-
-		#determine if the image is local or hosted
+		#determine if the image is local or hosted, then open it
 		if 'http' in self.source:
 			print 'reading from url'
 			file = cStringIO.StringIO(urllib.urlopen(self.source).read())
@@ -49,31 +45,42 @@ class ProcessImage(object):
 			except IOError:
 				print "Cannot load image. Be sure to include 'http://'' if loading from a website"
 				sys.exit()
+		self.imageWidth, self.imageHeight = self.image.size
 
+		try:
+			print '\033[95m' + "White: Pixel (0,0) color temp:", self.calcPixelTemp(self.image.getpixel((0,0)))
+		except:
+			print '\033[95m' + "White: Pixel (0,0) fails to calculate"
 
-		r,g,b = self.image.getpixel((1, 1))
-		self.results = self.calcPixelTemp(r,g,b)
+		try:
+			print '\033[91m' + "Red: Pixel (0,1) color temp:", self.calcPixelTemp(self.image.getpixel((0,1)))
+		except:
+			print '\033[91m' + "Red: Pixel (0,1) fails to calculate"
 
-	def calcPixelTemp(self,R,G,B):
+		try:
+			print '\033[94m' + "Blue: Pixel (1,0) color temp:", self.calcPixelTemp(self.image.getpixel((1,0)))
+		except:
+			print '\033[94m' + "Blue: Pixel (1,0) fails to calculate"
+
+		try:
+			print '\033[93m' + "Black: Pixel (1,1) color temp:", self.calcPixelTemp(self.image.getpixel((1,1)))
+		except:
+			print '\033[93m' + "Black: Pixel (1,1) fails to calculate"
+	def calcPixelTemp(self,RGB):
+		R,G,B=RGB
 		#http://dsp.stackexchange.com/questions/8949/how-do-i-calculate-the-color-temperature-of-the-light-source-illuminating-an-ima
-		#Convert the RGB values to CIE tristimulus 3D color space codinates
+		#Convert the RGB values to CIE tristimulus 3D color space coordinates
 		X = ((-0.14282) * B) + ((1.54924) * G) + ((-0.95641) * B)
 		Y = ((-0.32466) * R) + ((1.57837) * G) + ((-0.73191) * B) #illuminance
-		Z = ((-0.68202) * R) + ((0.77073) * G) + ((0.56332) * B)
-
+		Z = ((-0.68202) * R) + ((0.77073) * G) + (( 0.56332) * B)
 		#Compute the Combined Color Temperature
 		n=((0.23881) * R+(0.25499) * G+(-0.58291) * B) / ((0.11109) * R+(-0.85406) * G+(0.52289) * B)
 		CCT = (449 * (n**3)) + (3525 * (n**2) + (6823.3 * n) + 5520.33)
 		return CCT
-
 	def output(self): #Probably only print this in verbose mode in the future
-		print "We're processing the image: " + self.source
-		print "This " + self.image.mode + " image is in the " + self.image.format + " format"
-		print "Image Size: "
-		print self.image.size
-		print "Color Temp of First Pixel: "
-		print self.results
-
+		print '\033[0m' + "We're processing the image:" + self.source
+		print '\033[0m' + "This", self.image.mode, "image is in the", self.image.format, "format"
+		print '\033[0m' + "Image Size:", self.imageWidth, ",", self.imageHeight
 #Python needs this to instantiate the program properly when it's executed from the command line
 if __name__ == '__main__':
     main(sys.argv[1:])
