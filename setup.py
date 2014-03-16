8e4bdd668a54887e35597da43f5fab33092a62a3
Chris Knepper <cxk1108@rit.edu>
2014-03-16 19:32:08 -0400

    -Convert project to Python-distribution style -Move images to test directory

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+version = "0.1.0"
+
+setup(
+    name="Entropy",
+    version=version,
+    description="A tool to rate potential wallpapers",
+    keywords="color wallpaper images image",
+    packages=["entropy"],
+    author="Dustin Raimondi, Chris Knepper, Lance Laughlin",
+    author_email="chris82thekid@gmail.com",
+    url="http://www.github.com/ExplosiveHippo/Entropy",
+    license="LICENSE",
+    install_requires=[
+        "Pillow",
+    ]
+)
